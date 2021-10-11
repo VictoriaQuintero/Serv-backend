1dd22e7781d006c2a30b86bf123de95d6f1ef342
VictoriaQuintero <Victoria170460@gmail.com>
2021-10-11 13:34:16 -0400

    index.js

---
+++ funciones.js
@@ -0,0 +1,62 @@
+
+    function eliminarEdif(edif, res) {
+        encontrado = false;
+        ciudad.forEach(x => {
+            if (x.id === edif) {
+                ciudad.splice(ciudad.indexOf(x), 1);
+                encontrado = true;
+            }
+        });
+    
+        encontrado == false ? res.send("la edificacion que desea eliminar no existe") : res.send("se ha eliminado la edificacion: " + edif);
+    }
+    function editarEdif(edif, cont, res) {
+        let encontrado = false;
+        ciudad.forEach(x => {
+            if (x.id === edif) {
+                ciudad[ciudad.indexOf(x)] = cont;
+                ciudad[ciudad.indexOf(cont)].id = edif;
+                encontrado = true; 
+            }
+        });
+    
+        encontrado == false ? res.send("La edificacion que desea editar no existe") :  res.send(' Se edito, corretamente la edifición ' + edif);
+    }
+    
+    function mostrarEdif(tipo, res) {
+    
+        if (tipo.toLowerCase() === "casas") {
+            let casas = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "casa");
+            res.json(casas);
+        } else if (tipo.toLowerCase() === "edificios") {
+            let edificios = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "edificio");
+            res.json(edificios);
+        } else if (tipo.toLowerCase() === "carreteras") {
+            let carreteras = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "carretera");
+            res.json(carreteras);
+        } else if (tipo.toLowerCase() === "puentes") {
+            let puentes = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "puente");
+            res.json(puentes);
+        } else if (tipo.toLowerCase() === "parques") {
+            let parques = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "parque");
+            res.json(parques);
+        } else {
+            res.send("La edificacion que desea no existe");
+        }
+    }
+    
+    function MostrarPorId(id, info, res) {
+        let encontrado = false;
+        ciudad.forEach(edif => {
+            if (edif.id === id) {
+                res.json(edif);
+                encontrado = true;
+            }
+        });
+        encontrado == false ? res.send("La edificacion que busca no existe") :  res.json(info);
+    }
+
+    module.exports.editarEdif = editarEdif;
+    module.exports.eliminarEdif = eliminarEdif;
+    module.exports.mostrarEdif = mostrarEdif;
+    module.exports.MostrarPorId = MostrarPorId;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const funciones = require('./funciones')
+
 app.use(express.json());
+
+
 
 ciudad = [
     {
@@ -21,85 +25,55 @@ ciudad = [
         'altura': '100m',
         'estado': 'nueva',
         "id": "2"
+    },
+    {
+        "tipo" : "parque",
+        "ubicacion" : "este",
+        "columpios" : "3",
+        "id" : "3"
+    },
+    {
+        "tipo" : "edificio",
+        "color" : "negro",
+        "tamaño" : "50m",
+        "id" : "4"
     }
 ]
 
-function eliminarEdif(edif) {
-    ciudad.forEach(x => {
-        if (x.id === edif) {
-            ciudad.splice(ciudad.indexOf(x), 1);
-        }
-    });
-}
-function editarEdif(edif, cont) {
-    ciudad.forEach(x => {
-        if (x.id === edif) {
-            ciudad[ciudad.indexOf(x)] = cont;
-            ciudad[ciudad.indexOf(cont)].id = edif;
-        }
-    });
-}
-
-function mostrarEdif(tipo, res) {
-
-    if(tipo.toLowerCase() === "casas"){
-        let casas = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "casa");
-        res.json(casas);
-    }else if(tipo.toLowerCase() === "edificios"){
-        let edificios = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "edificio");
-        res.json(edificios);
-    }else if(tipo.toLowerCase() === "carreteras"){
-        let carreteras = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "carretera");
-        res.json(carreteras);
-    }else if(tipo.toLowerCase() === "puentes"){
-        let puentes = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "puente");
-        res.json(puentes);
-    }else if(tipo.toLowerCase() === "parques"){
-        let parques = ciudad.filter(estructura => estructura.tipo.toLowerCase() === "parque");
-        res.json(parques);
-    }else{
-        res.send("La edificacion que desea no existe");
-    }
-}
-
-
-
-function MostrarPorId(id, res) {
-    ciudad.forEach(edif => {
-        if (edif.id === id) {
-            res.json(edif);
-        }
-    })
-}
+//metodo get para mostrar la ciudad completa
 app.get("/", (req, res) => {
     res.json(ciudad);
 });
+
+//metodo get para saber la cantidad de edificaciones
+app.get('/cantidad', (req, res) =>{
+    res.send("la cantidad de edificaciones construidas es: " + ciudad.length);
+})
+
+//metodo post para agregar alguna edificación 
 app.post('/agregar/:id', (req, res) => {
     ciudad.push(req.body);
     ciudad[ciudad.length - 1].id = req.params.id;
     res.send("Edificacion recibida Correctamente, la edificacion esta en el puesto: " + req.params.id + " si desea editarla debe utilizar ese id");
 });
 
+//metodo put para editar alguna edificacion 
 app.put('/editar/:id', (req, res) => {
-    let a = req.params.id;
-    let elim = ciudad[parseInt(a)];
-    editarEdif(a, req.body);
-    res.send(' se edito, ' + elim + ' por: ' + req.body);
+    funciones.editarEdif(req.params.id, req.body, res);
 });
 
+//metodo delete para eliminar alguna edificacion 
 app.delete('/eliminar/:id', (req, res) => {
-    let b = req.params.id;
-    let elim = ciudad[parseInt(b)];
-    eliminarEdif(b);
-    res.send("se ha eliminado la siguiente edificacion: " + elim);
+    funciones.eliminarEdif(req.params.id, res);
 })
-
+//metodo get para mostrar un tipo de edificacion por grupos
 app.get('/ciudad/:tipo', (req, res) => {
-    mostrarEdif(req.params.tipo, res);
+    funciones.mostrarEdif(req.params.tipo, res);
 });
+
+//metodo get para mostrar edificaciones especificas por el id 
 app.get('/edificaciones/:id', (req, res) => {
-    let edificacion = req.params.id;
-    mostrarEdif
+    funciones.MostrarPorId(req.params.id, req.body, res);
 
 })
 
